d50a1b356c6500a31cab6e05afd7f8faf49b38f9
ilSyAbRi <iliasssyabri1@gmail.com>
2026-05-26 18:09:03 +0100

    get the right i

---
+++ enumerate/two_list_and_enumerate.py
@@ -0,0 +1,11 @@
+raw = ["nb_drones: 2", "", "# comment", "hub: roof1", ""]
+clean = ["nb_drones: 2", "hub: roof1"]
+
+last_list = []
+
+for i , v in enumerate(raw):
+    for e in clean:
+        if e == v:
+            last_list.append((i,e))
+
+print(last_list)
