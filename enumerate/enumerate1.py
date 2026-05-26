9cb733716e57cae31ff83f78edb181a5c7dc6abc
ilSyAbRi <iliasssyabri1@gmail.com>
2026-05-26 17:59:39 +0100

    find with enumerate

--- enumerate/enumerate1.py
+++
@@ -1,9 +0,0 @@
-
-def main():
-    a = {"c":10, "b":20}
-    for i , (k, v) in enumerate(a.items()):
-        print(i, k , v)
-
-if __name__ == "__main__":
-    main()
-
---
+++ enumerate/find_with_enumerate.py
@@ -0,0 +1,5 @@
+words = ["hello", "world", "foo", "bar"]
+
+for i , v in enumerate(words):
+    if v == "foo":
+        print(i, v)
