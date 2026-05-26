ff174193c53f72c00111615f9c2ffa13e2fc55e4
ilSyAbRi <iliasssyabri1@gmail.com>
2026-05-26 19:35:37 +0100

    improve code

--- enumerate/two_list_and_enumerate.py
+++ enumerate/two_list_and_enumerate.py
@@ -1,11 +1,5 @@
 raw = ["nb_drones: 2", "", "# comment", "hub: roof1", ""]
 clean = ["nb_drones: 2", "hub: roof1"]
 
-last_list = []
-
-for i , v in enumerate(raw,start=1):
-    for e in clean:
-        if e == v:
-            last_list.append((i,e))
-
+last_list = [(i,v) for i, v in enumerate(raw, start=1) if v in clean]
 print(last_list)
